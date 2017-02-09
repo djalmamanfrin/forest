2bc987eda6667b0a7a69c28cbf92595f372a8fcc
Djalma Manfrin <djalma.manfrin@gmail.com>
2017-02-09 13:59:00 -0200

    Config composer-json

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,40 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    /**
+     * @param $slug
+     * @return string
+     */
+    public function slug($slug){
+        return str_slug($slug);
+    }
 }
