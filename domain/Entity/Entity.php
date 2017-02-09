4ba9d74924068af1e98593e529064fc0efec6938
Djalma Manfrin <djalma.manfrin@gmail.com>
2017-02-09 14:02:25 -0200

    Apply folders Domain

---
+++ domain/Entity/Entity.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Class Entity
+ * @package Domain\Entity\Entity
+ */
+namespace Domain\Entity;
+
+use Carbon\Carbon;
+
+class Entity
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var Carbon;
+     */
+    private $created_at;
+
+    /**
+     * @var Carbon
+     */
+    private $updated_at;
+
+    /**
+     * @var Carbon
+     */
+    private $deleted_at;
+
+    /**
+     * @return mixed
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return self
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getCreatedAt(): Carbon
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param Carbon $created_at
+     * @return self
+     */
+    public function setCreatedAt(Carbon $created_at): self
+    {
+        $this->created_at = $created_at;
+        return $this;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getUpdatedAt(): Carbon
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param Carbon $updated_at
+     * @return self
+     */
+    public function setUpdatedAt(Carbon $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+        return $this;
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function getDeletedAt(): Carbon
+    {
+        return $this->deleted_at;
+    }
+
+    /**
+     * @param Carbon $deleted_at
+     * @return self
+     */
+    public function setDeletedAt(Carbon $deleted_at): self
+    {
+        $this->deleted_at = $deleted_at;
+        return $this;
+    }
+}
---
+++ tests/domain/Entity/EntityTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Class EntityTest
+ * @package Tests\Domain\EntityTest
+ */
+namespace Tests\Domain\EntityTest;
+
+use Carbon\Carbon;
+use Domain\Entity\Entity;
+use Tests\TestCase;
+
+class EntityTest extends TestCase
+{
+    /**
+     * Check if exists entity class
+     */
+    public function testEntityClassExists()
+    {
+        $class = $this->getMockBuilder(Entity::class)->getMock();
+
+        $this->assertInstanceOf(Entity::class, $class);
+    }
+
+    /**
+     * Check the get and set methods in entity class
+     */
+    public function testEntityMethodsExists()
+    {
+        $entity = $this->getMockForAbstractClass(Entity::class);
+
+        $this->assertTrue(method_exists($entity, 'getId'));
+        $this->assertTrue(method_exists($entity, 'setId'));
+
+        $this->assertTrue(method_exists($entity, 'getCreatedAt'));
+        $this->assertTrue(method_exists($entity, 'setCreatedAt'));
+
+        $this->assertTrue(method_exists($entity, 'getUpdatedAt'));
+        $this->assertTrue(method_exists($entity, 'setUpdatedAt'));
+
+        $this->assertTrue(method_exists($entity, 'getDeletedAt'));
+        $this->assertTrue(method_exists($entity, 'setDeletedAt'));
+    }
+
+    public function testSetReturnAnEntity()
+    {
+        $entity = $this->getMockForAbstractClass(Entity::class);
+
+        $this->assertInstanceOf(Entity::class, $entity->setId(1));
+        $this->assertInstanceOf(Entity::class, $entity->setCreatedAt(Carbon::now()));
+        $this->assertInstanceOf(Entity::class, $entity->setUpdatedAt(Carbon::now()));
+        $this->assertInstanceOf(Entity::class, $entity->setDeletedAt(Carbon::now()));
+    }
+}
