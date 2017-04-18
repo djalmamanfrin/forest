a44242dac4fe79a23c175510e96c892cbcd395a2
Djalma Manfrin <djalma.manfrin@gmail.com>
2017-04-18 10:31:53 -0300

    config files

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class Request
+ * @package App\Http\Request
+ */
+abstract class Request extends FormRequest
+{
+
+}
---
+++ app/Http/Routes/V1.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Http\Routes;
+
+use Illuminate\Contracts\Routing\Registrar;
+
+/**
+ * Class V1
+ * @package App\Http\Routes
+ */
+class V1
+{
+    /**
+     * Controllers should be in 'App\Http\Controllers\V1'
+     * @param Registrar $router
+     */
+    public function map(Registrar $router)
+    {
+    }
+}
